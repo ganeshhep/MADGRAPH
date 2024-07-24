9b9a23dcfc6eb78cfa8be519cf732eee554065f0
Ganesh Hasda <156532313+ganeshhep@users.noreply.github.com>
2024-07-24 16:41:04 +0530

    Add files via upload

--- Analyzer2.C
+++ Analyzer2.C
@@ -32,17 +32,17 @@ void Analyzer2(const char *inputFile)
 
   // Book histograms
   TH1 *histJetN = new TH1F("number of jets", "Jet multiplicity", 7, 1, 8);
-  TH1 *histJetInvM = new TH1F("invaiant mass", "M_{inv}(jet_{1}, jet_{2})", 130, 10.0, 140.0);
+  TH1 *histJetInvM = new TH1F("invaiant mass", "M_{inv}(jet_{1}, jet_{2})", 130, 10.0, 200.0);
 
   // Loop over all events
   for(Int_t entry = 0; entry < numberOfEntries; ++entry)
   {
-    double ref_pt = 0, jet_pt, jet_mass_1, jet_mass_2, jet_pt_1, jet_pt_2;
+    double ref_pt_1 = 0, ref_pt_2 = 0, j1_pt, j2_pt, jet_pt_1;
     Int_t k = 0;
     // Load selected branches with data from specified event
     treeReader->ReadEntry(entry);
 
-    Jet *jet_b;
+    Jet *jet_b, *j1, *j2, *B_j1, *B_j2;
 
     // Number of b-tagged jets
     for(Int_t i = 0; i < branchJet->GetEntries(); i++){
@@ -52,39 +52,38 @@ void Analyzer2(const char *inputFile)
       }
     }
 
-    if(branchJet->GetEntries() > 1) // If event contains at least 2 jet
+    if(k > 1) // If event contains at least 2 b-tagged jets
     {
-      
-      Jet *jet, *jet_2;
-
       for(Int_t j = 0; j < branchJet->GetEntries(); j++){
-        jet = (Jet *) branchJet->At(j);
-        jet_pt = jet->PT;
-	  if(jet->BTag == 1 && k > 1){
-            if(jet_pt > ref_pt){
-              ref_pt = jet_pt;
-	      jet_mass_1 = (jet->P4()).M();
-              jet_pt_1 = jet_pt;
+        j1 = (Jet *) branchJet->At(j);
+        j1_pt = j1->PT;
+	  if(j1->BTag == 1){
+            if(j1_pt > ref_pt_1){
+              ref_pt_1 = j1_pt;
+	      //jet_mass_1 = (jet->P4()).M();
+              jet_pt_1 = j1_pt;
+              B_j1 = j1;
 	    }
           }
       }
 
        for(Int_t l = 0; l < branchJet->GetEntries(); l++){
-        jet_2 = (Jet *) branchJet->At(l);
-        jet_pt_2 = jet_2->PT;
-	  if(jet_2->BTag == 1 && k > 1){
-            if(jet_pt_2 < jet_pt_1 && jet_pt_2 > ref_pt){
-              ref_pt = jet_pt_2;
-	      jet_mass_2 = (jet_2->P4()).M();
+        j2 = (Jet *) branchJet->At(l);
+        j2_pt = j2->PT;
+	  if(j2->BTag == 1){
+            if(j2_pt < jet_pt_1 && j2_pt > ref_pt_2){
+              ref_pt_2 = j2_pt;
+	      //jet_mass_2 = (jet_2->P4()).M();
+              B_j2 = j2;
 	    }
           }
       }
-
-
+   histJetInvM->Fill((B_j1->P4() + B_j2->P4()).M());
+      
 
     }
     
-    histJetInvM->Fill(jet_mass_1 + jet_mass_2);
+    
   } 
 
   // Show resulting histograms
